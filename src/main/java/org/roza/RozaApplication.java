478ba7294244a520c0545102e493cc4c4c5b5c6a
stoyanoffz <hristostoyanoff@gmail.com>
2019-11-15 08:21:41 +0200

    init commit

---
+++ src/main/java/org/roza/RozaApplication.java
@@ -0,0 +1,13 @@
+package org.roza;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RozaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RozaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/roza/RozaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.roza;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RozaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
